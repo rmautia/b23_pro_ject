c2617e00c2a5a3dc6579bc45b486f5cbeeaac627
rmautia <raphaelnyangenya@gmail.com>
2021-07-19 23:42:48 +0300

    Adding github to intellij

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,4 +1,7 @@
 public class GitCool {
     // B23 first UI experience with Git
     // B23 has a special number
+
+    // Adding a Github account under the Intellij IDE
+    // make sure to click on apply and click to save
 }
