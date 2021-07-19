88a389250bf7e599aa12dd0c55a00f744f71ba88
rmautia <raphaelnyangenya@gmail.com>
2021-07-19 23:21:17 +0300

    adding awesome code message

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Adding some awesome codes here");
     }
 
 }
