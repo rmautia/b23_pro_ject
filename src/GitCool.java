185310d1c3adfe3763a0983540ad3a9e059d7688
rmautia <raphaelnyangenya@gmail.com>
2021-07-19 23:44:55 +0300

    Additional info on link from local to remote

--- src/GitCool.java
+++ src/GitCool.java
@@ -4,4 +4,9 @@ public class GitCool {
 
     // Adding a Github account under the Intellij IDE
     // make sure to click on apply and click to save
+
+    // successfully deploying a project to github
+    // sharing a project to Github happens only once
+    // Create remote repository under your account
+    // push local commits to remote repo
 }
